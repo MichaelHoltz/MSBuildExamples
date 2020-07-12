6d4e28ccd01dbc3b11b9a6870c783361b6997678
MichaelHoltz <me@mikeholtz.com>
2020-07-12 12:15:22 -0700

    Adding actual console application for testing

---
+++ Templates/20_FrameworkConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace FrameworkConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World of MSBuild");
+            Object[] ca = System.Reflection.Assembly.GetEntryAssembly().GetCustomAttributes(false);
+            foreach (var item in ca)
+            {
+                if (item.GetType().FullName == "System.Runtime.Versioning.TargetFrameworkAttribute")
+                {
+                    System.Runtime.Versioning.TargetFrameworkAttribute t = (System.Runtime.Versioning.TargetFrameworkAttribute)item;
+                    Console.WriteLine("Current .NET framework is: " + t.FrameworkDisplayName);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
