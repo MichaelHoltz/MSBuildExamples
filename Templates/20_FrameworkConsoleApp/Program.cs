9a70111fdb32731b91b36bfe32af9fd9cc429872
MichaelHoltz <me@mikeholtz.com>
2020-07-12 13:43:10 -0700

    Modified to generate a file with the Bamboo Build Number and code to display it when building like: dotnet msbuild -p:TargetFrameworkVersion="v4.6.2" -p:BambooBuildNumber=${bamboo.buildNumber}

--- Templates/20_FrameworkConsoleApp/Program.cs
+++ Templates/20_FrameworkConsoleApp/Program.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.IO;
+//using Newtonsoft.json;
 
 namespace FrameworkConsoleApp
 {
@@ -20,6 +21,15 @@ namespace FrameworkConsoleApp
                     Console.WriteLine("Current .NET framework is: " + t.FrameworkDisplayName);
                 }
             }
+            if(File.Exists("BambooInfo.txt")){
+                String bambooInfo = File.ReadAllText("BambooInfo.txt");
+                Console.WriteLine("Bamboo Build Number = " + bambooInfo);
+            }
+
+            
+
+            Console.WriteLine("Press Any Key to continue");
+            //Keep the console open to be able to see what it says if not run from a Command Window
             Console.ReadLine();
         }
     }
